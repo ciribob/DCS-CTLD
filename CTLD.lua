baf1231141d82b28cc1d593d65e6e42e272a0d2e
FullGas1 <a.lingo@laposte.net>
2025-03-25 14:57:32 +0100

    fixes

--- CTLD.lua
+++ CTLD.lua
@@ -8076,8 +8076,8 @@ function ctld.initialize()
             timer.scheduleFunction(ctld.checkHoverStatus, nil, timer.getTime() + 1)
         end
         if ctld.enableRepackingVehicles == true then
-            --timer.scheduleFunction(ctld.autoUpdateRepackMenu, nil, timer.getTime() + 3)
-            --timer.scheduleFunction(ctld.repackVehicle, nil, timer.getTime() + 1)
+            timer.scheduleFunction(ctld.autoUpdateRepackMenu, nil, timer.getTime() + 3)
+            timer.scheduleFunction(ctld.repackVehicle, nil, timer.getTime() + 1)
         end
     end, nil, timer.getTime() + 1)
 
